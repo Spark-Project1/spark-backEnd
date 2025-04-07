690efb044c952b6069152769789de140955dbaad
kimyeongeuk <tklr0731@naver.com>
2025-04-07 18:37:19 +0900

    Update SecurityConfig.java

--- spark/src/main/java/com/sp/boot/config/SecurityConfig.java
+++ spark/src/main/java/com/sp/boot/config/SecurityConfig.java
@@ -45,19 +45,7 @@ public class SecurityConfig {
    
    
    
-   @Configuration
-   public class WebConfig implements WebMvcConfigurer {
 
-       @Override
-       public void addCorsMappings(CorsRegistry registry) {
-           // 모든 엔드포인트에 대해 CORS 적용
-           registry.addMapping("/**") // 모든 경로로 설정
-               .allowedOrigins("http://localhost:3000")  // React 앱의 주소
-               .allowedMethods("GET", "POST", "PUT", "DELETE")  // 허용할 HTTP 메서드
-               .allowedHeaders("*")  // 모든 헤더 허용
-               .allowCredentials(true);  // 자격 증명 (예: 쿠키) 허용
-       }
-   }
    
    
    
