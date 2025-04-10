616b1e011e0793354cd9226d38c348ede1b9e34b
parkpoto0404 <parkpoto@gmail.com>
2025-04-10 14:55:34 +0900

    dao , mapper 로직변경

--- spark/src/main/java/com/sp/boot/dao/MemberDao.java
+++ spark/src/main/java/com/sp/boot/dao/MemberDao.java
@@ -1,10 +1,9 @@
 package com.sp.boot.dao;
 
-import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 import com.sp.boot.dto.MemberDto;
+import com.sp.boot.mapper.MemberMapper;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,17 +11,25 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class MemberDao {
 	
-	private final SqlSessionTemplate sqlSession;
+	//private final SqlSessionTemplate sqlSession;
+	private final MemberMapper memberMapper;
 
-	public MemberDto login(MemberDto m) {
-		return sqlSession.selectOne("memberMapper.loginMember",m);
-	}
+	/*
+	 * public MemberDto login(MemberDto m) { return
+	 * sqlSession.selectOne("memberMapper.loginMember",m); }
+	 * 
+	 * public MemberDto loginUserInfo(String userId) { return
+	 * sqlSession.selectOne("memberMapper.loginUserInfo",userId); }
+	 */
+	
 
-	public MemberDto loginUserInfo(String userId) {
-		return sqlSession.selectOne("memberMapper.loginUserInfo",userId);
-	}
-	
-	
+    public MemberDto login(MemberDto m) {
+        return memberMapper.loginMember(m);
+    }
+
+    public MemberDto loginUserInfo(String memId) {
+        return memberMapper.loginUserInfo(memId);
+    }
 	
 	
 	
---
+++ spark/src/main/java/com/sp/boot/mapper/MemberMapper.java
@@ -0,0 +1,14 @@
+package com.sp.boot.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.sp.boot.dto.MemberDto;
+
+@Mapper
+public interface MemberMapper {
+	
+	 MemberDto loginMember(MemberDto m);
+	 
+	 MemberDto loginUserInfo(String userId);
+
+}
