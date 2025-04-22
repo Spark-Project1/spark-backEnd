f10f3a377c840631f3a695ae478816f236c57866
kimyeongeuk <tklr0731@naver.com>
2025-04-22 15:13:57 +0900

    좋아요 dto 추가

---
+++ spark/src/main/java/com/sp/boot/dto/LikeDto.java
@@ -0,0 +1,26 @@
+package com.sp.boot.dto;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@ToString
+public class LikeDto {
+	
+	private int likeNo;
+	private String requestId;
+	private String responseId;
+	private String status;
+	
+
+}
