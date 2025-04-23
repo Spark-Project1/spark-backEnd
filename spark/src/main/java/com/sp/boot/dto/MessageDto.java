a8a2e50805ddf67e955dd4b7db0c8cf91c2ad1d6
kimyeongeuk <tklr0731@naver.com>
2025-04-23 17:55:52 +0900

    채팅 추가

---
+++ spark/src/main/java/com/sp/boot/dto/MessageDto.java
@@ -0,0 +1,30 @@
+package com.sp.boot.dto;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@ToString
+public class MessageDto {
+
+	private int msgNo;
+	private String msgContent;
+	private Date msgRegistDate;
+	private String msgId;
+	private int clNo;
+	private String chatTitle;
+	private String nickName;
+	private String proFile;
+	
+	
+}
